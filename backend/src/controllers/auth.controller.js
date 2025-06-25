038405389b81830917f6928f932cf113c1b52fa6
Digvijay-x1 <hackerearthx1@gmail.com>
2025-06-25 18:16:36 +0530

    add the check-auth in the routers and controllers

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -78,3 +78,13 @@ export const updateProfile = async(req , res)=>{
         res.status(500).json({message: "Internal server error in auth controller" + error.message});
     }
 }
+
+export const checkAuth = async(req , res)=>{
+    try {
+        const user = req.user;
+        res.status(200).json({message: "User is authenticated", user});
+    } catch (error) {
+        console.log(`Error in checkAuth controller ${error.message}`);
+        res.status(500).json({message: "Internal server error in auth controller" + error.message});
+    }
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { login, signup , logout , updateProfile} from "../controllers/auth.controller.js";
+import { login, signup , logout , updateProfile , checkAuth} from "../controllers/auth.controller.js";
 import { protectRoute } from "../middlewares/auth.middleware.js";
 const router = express.Router();
 
@@ -10,6 +10,8 @@ router.post('/login', login)
 router.post('/logout', logout)
 
 router.put('/update-profile', protectRoute , updateProfile)
+
+router.get('/check-auth', protectRoute , checkAuth)
 
 
 export default router;
