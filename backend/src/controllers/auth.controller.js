70f4794fb3b1bed4340c6b9856bb3e030f39d0e3
Digvijay-x1 <hackerearthx1@gmail.com>
2025-06-25 17:32:03 +0530

    add the login and logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs'
 import { generateToken } from "../lib/utils.js";
 
+
 export const signup = async (req , res)=>{
     const {name , email , password} = req.body;
 
@@ -26,10 +27,38 @@ export const signup = async (req , res)=>{
     }
 }
 
-export const login = (req , res)=>{
-    res.send("login route");
+export const login = async(req , res)=>{
+    const {email , password} = req.body;
+    try {
+        const existingUser = await User.findOne({email});
+        if(!existingUser) return res.status(400).json({message: "Invalid credentials"});
+
+        // compare password
+
+        bcrypt.compare(password , existingUser.password , (err , isMatch)=>{
+            if(err) return res.status(500).json({message: "Internal server error in auth controller" + err.message});
+
+            if(!isMatch) return res.status(400).json({message: "Invalid credentials"});
+
+            const token = generateToken(existingUser._id , res);
+            res.status(200).json({message: "User logged in successfully", user: existingUser , token});
+        })
+    } catch (error) {
+        console.log(`Error in login controller ${error.message}`);
+        res.status(500).json({message: "Internal server error in auth controller" + error.message});
+    }
 }
 
 export const logout = (req , res)=>{
-    res.send("logout route");
+    try {
+        res.cookie("jwt" , "" , {maxAge: 0});
+        res.status(200).json({message: "User logged out successfully"});
+    } catch (error) {
+        console.log(`Error in logout controller ${error.message}`);
+        res.status(500).json({message: "Internal server error in auth controller" + error.message});
+    }
+}
+
+export const updateProfile = async(req , res)=>{
+
 }
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoute from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({limit: "10mb"}));
+app.use(cookieParser());
 
 app.use('/api/auth' , authRoute)
 
